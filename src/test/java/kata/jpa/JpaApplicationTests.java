e6da164fb2c37b7fe1e1d3486e018d17d758c780
Rafael Santos <rafa.can.dev@gmail.com>
2023-02-17 16:13:04 -0500

    Spring boot init

---
+++ src/test/java/kata/jpa/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package kata.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
