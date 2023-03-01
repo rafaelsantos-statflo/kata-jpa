cdafe0621822c614d5f191a4b0de928494db3081
Rafael Santos <rafael.santos@statflo.com>
2023-03-01 13:04:44 -0500

    Refactor tests

--- src/test/java/kata/jpa/address/AddressRepositoryTest.java
+++ src/test/java/kata/jpa/address/AddressRepositoryTest.java
@@ -1,66 +1,64 @@
 package kata.jpa.address;
 
+import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import kata.jpa.address.AddressRepository;
-import lombok.val;
 import org.springframework.dao.DataIntegrityViolationException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class AddressTest {
+class AddressRepositoryTest {
+	private static final String LINE_ONE = "123 ABC Avenue";
+	private static final String LINE_TWO = "Unit 2";
 
 	@Autowired
 	private AddressRepository addressRepository;
 
 	@Test
-	void saveAddress() {
+	void save_Given_NewAddress_Then_Success() {
 		val address = new Address();
+		address.setLineOne(LINE_ONE);
 		addressRepository.save(address);
 		val actual =  addressRepository.findById(address.getId());
-
 		assertEquals(actual.get(), address);
 	}
 
 	@Test
-	void saveAddressWithLineOne() {
+	void save_Given_AddressWithLineOne_Then_Success() {
 		val address = new Address();
-		address.setLineOne("123 ABC Avenue");
+		address.setLineOne(LINE_ONE);
 		addressRepository.save(address);
 		val actual =  addressRepository.findById(address.getId());
-
 		assertEquals(actual.get(), address);
 	}
 
 	@Test
-	void saveAddressWithoutMandatoryFields() {
+	void save_Given_AddressWithLineTwo_Then_Success(){
 		val address = new Address();
-		assertThrows(DataIntegrityViolationException.class, () -> addressRepository.save(address));
-	}
-
-	@Test
-	void saveAddressWithLineTwo(){
-		val address = new Address();
-		address.setLineOne("123 ABC Avenue");
-		address.setLineTwo("Unit 2");
+		address.setLineOne(LINE_ONE);
+		address.setLineTwo(LINE_TWO);
 		addressRepository.save(address);
 		val actual= addressRepository.findById(address.getId());
-
 		assertEquals(actual.get(), address);
 	}
 
 	@Test
-	void findByLineOne(){
+	void save_Given_MissingMandatoryFields_Then_Exception() {
 		val address = new Address();
-		val lineOne = "123 ABC Avenue";
+		val actual = assertThrows(DataIntegrityViolationException.class, () -> addressRepository.save(address));
+		assertEquals("not-null property references a null or transient value : kata.jpa.address.Address.lineOne", actual.getMessage());
+	}
+
+	@Test
+	void findByLineOne_Given_ExistingAddress_Then_Success(){
+		val address = new Address();
+		val lineOne = LINE_ONE;
 		address.setLineOne(lineOne);
 		addressRepository.save(address);
 		val actual = addressRepository.findByLineOne(lineOne);
 		assertTrue(actual.contains(address));
 	}
-
 
 }
