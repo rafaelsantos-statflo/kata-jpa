d02c847ecc863870b35882b54cc68699c44a9f2b
fayaz.fahim <fayazfahim1@gmail.com>
2023-04-05 13:59:10 -0400

    Add Variant to JPA

--- src/main/java/kata/jpa/product/Variant.java
+++ src/main/java/kata/jpa/product/Variant.java
@@ -1,34 +1,21 @@
 package kata.jpa.product;
 
+
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.repository.cdi.Eager;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Setter
 @Getter
 @EqualsAndHashCode
-public class Product {
+public class Variant {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Integer id;
 
     private String name;
-
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER )
-    private List<Variant> variants = new ArrayList<>();
-
-
-    public void addVariant(Variant variant){
-        variants.add(variant);
-    }
-
-
 }
--- src/test/java/kata/jpa/product/ProductRepositoryTest.java
+++ src/test/java/kata/jpa/product/ProductRepositoryTest.java
@@ -4,6 +4,7 @@ import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,4 +26,23 @@ public class ProductRepositoryTest {
         assertEquals(actual.get(), product);
 
     }
+
+    @Test
+    void save_Given_NewProduct_WithVariant_Then_Success() {
+        Product product = new Product();
+        String productName = "shirt";
+        product.setName(productName);
+        Variant variant = new Variant();
+        variant.setName("pink");
+        product.addVariant(variant);
+
+        productRepository.save(product);
+
+        val actual =  productRepository.findById(product.getId());
+        assertEquals(actual.get().getId(), product.getId());
+        assertEquals(1,product.getVariants().size());
+
+    }
+
+
 }
